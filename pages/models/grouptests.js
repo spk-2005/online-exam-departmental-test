18ecfbd7646e89b51ef5d6262cb121c3cab0af7d
spk-2005 <prasannasimha5002@gmail.com>
2025-07-08 21:55:44 +0530

    test-commit-1

--- pages/models/grouptests.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const GroupTestSchema = new mongoose.Schema({
-  group: { type: String, required: true, unique: true },
-  tests: [{ type: String, required: true }],
-});
-
-module.exports = mongoose.models.GroupTest || mongoose.model("GroupTest", GroupTestSchema);
