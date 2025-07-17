ec057424af59d2c338b6dd757e64bdca34585e7e
spk-2005 <prasannasimha5002@gmail.com>
2025-07-17 19:54:04 +0530

    test-commit-1

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,31 @@
 // pages/_app.js
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import { initGA, logPageView } from "../lib/ga";
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    initGA();
-    logPageView(window.location.pathname);
-
-    router.events.on("routeChangeComplete", logPageView);
-    return () => {
-      router.events.off("routeChangeComplete", logPageView);
-    };
-  }, [router.events]);
-
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics (replace G-XXXXXXX with your actual Measurement ID) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-5194FX0QZR"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5194FX0QZR', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
